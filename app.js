73154d2a35fa16c394dbd719c5239ca644b35646
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2013-05-29 12:36:21 -0400

    Simplify app.js

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
 // Bring in all your require modules
 var express = require( "express" ),
-    habitat = require( "habitat" ),
     nunjucks = require( "nunjucks" ),
     path = require( "path" ),
-    i18n = require( "./lib/i18n.js" );
-
-// Load config from ".env"
-habitat.load();
-
-// Generate app variables
-var app = express(),
-    env = new habitat(),
-    middleware = require( "./lib/middleware" ),
-    nunjucksEnv = new nunjucks.Environment( new nunjucks.FileSystemLoader( path.join( __dirname + '/views' ))),
+    i18n = require( "./lib/i18n.js" ),
+    app = express(),
+    nunjucksEnv = new nunjucks.Environment( new nunjucks.FileSystemLoader( path.join( __dirname, '/views' ))),
     routes = require( "./routes" );
 
 // Enable template rendering with nunjucks
 nunjucksEnv.express( app );
-// Don't send the "X-Powered-By: Express" header
-app.disable( "x-powered-by" );
 
-// Setup global middleware
 app.use( express.logger());
-// Always put the compression middleware high up in the chain
-app.use( express.compress());
+
 // Setup locales with i18n
 app.use( i18n.abide({
   supported_languages: [ "db_LB", "de", "en_US", "es" ],
@@ -31,25 +19,17 @@ app.use( i18n.abide({
   translation_directory: "locale",
   localeOnUrl: true
 }));
+// Dump locale info to console
 app.use( function( req, res, next ) {
   console.log( "Using locale: %s", req.lang);
   next();
 });
 app.use( "/static", express.static( path.join( __dirname + "/static" )));
-// bodyParser will parse "application/json", "application/x-www-form-urlencoded" and "multipart/form-data"
-// requests and put the results on req.body and req.files. Handy!
-// If you don't need to handle all three types then just use json(), urlencoded() or multipart() instead.
-app.use( express.bodyParser());
-// Attempt to use Express' routes defined below
-app.use( app.router );
-// Whenever you pass `next( someError )`, this middleware will handle it
-app.use( middleware.errorHandler );
 
-// Express routes
 app.get( "/", routes.index );
 app.get( "/data", routes.data );
 
 // Start up the server
-app.listen( env.get( "PORT", 3000 ), function() {
-  console.log( "Server listening (Probably http://localhost:%d )", env.get( "PORT", 3000 ));
+app.listen( 3000, function() {
+  console.log( "Server listening (http://localhost:3000)" );
 });
