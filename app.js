a353b16752d990f5a13f093df9d1ec3ef4d0a507
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2013-05-30 17:05:44 -0400

    Update supported langauges in app.js

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ app.use( express.logger());
 
 // Setup locales with i18n
 app.use( i18n.abide({
-  supported_languages: [ "db_LB", "de", "en_US", "es" ],
+  supported_languages: [
+    'ach', 'ady', 'af', 'am', 'an', 'ar', 'as', 'ast', 'az', 'be', 'be_BY', 'bg', 'bn', 'bo',
+    'br', 'bs', 'ca', 'cs', 'cv', 'cy', 'da', 'da_DK', 'el', 'eo', 'et', 'eu', 'fa', 'fi',
+    'fil', 'fr', 'ga', 'gd', 'he', 'hi', 'hr', 'ht', 'hu', 'hy', 'ia', 'id', 'is', 'it',
+    'ja', 'ja_JP', 'jv', 'ka', 'kk', 'km', 'kn', 'ko', 'ku', 'la', 'lb', 'lt', 'lv', 'mi',
+    'mk', 'ml', 'mn', 'mr', 'ms', 'mt', 'my', 'ne', 'nl', 'no', 'nr', 'or', 'os', 'pa', 'pl',
+    'pt', 'ro', 'ru', 'rw', 'sc', 'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sq', 'sq_AL', 'sr',
+    'sv', 'sw', 'ta', 'te', 'th_TH', 'ti', 'tl', 'tn', 'to', 'tr', 'tt', 'uk', 'ur', 'uz',
+    'vec', 'vi', 'wa', 'wo', 'xh', 'yi', 'yo', 'zh', 'zu', 'db_LB', 'en_US'
+  ],
   default_lang: "en_US",
   translation_directory: "locale",
   localeOnUrl: true
